8b90da5d78c0ab41f318d249226f9593bf88797d
EragonJ <eragonj@eragonj.me>
2016-06-22 18:22:39 +0800

    add unit test

---
+++ tests/setup.js
@@ -0,0 +1,14 @@
+'use strict';
+
+var assert = require('assert');
+var chai = require('chai');
+var sinon = require('sinon');
+var jsdom = require('jsdom').jsdom;
+var proxyquire = require('proxyquire').noCallThru();
+
+// We have to fake window object in node.js environment
+var document = jsdom('_');
+global.window = document.defaultView;
+global.assert = chai.assert;
+global.sinon = sinon;
+global.proxyquire = proxyquire;
